3001f09b34e0ca9ea8256cf80c82b2efe0a6894a
jordanni11 <jordanni.santana@lc.cuny.edu>
2024-10-04 20:17:34 -0400

    Add files via upload

---
+++ Project Mat 327.R
@@ -0,0 +1,32 @@
+View(Expense_Budget_20240918)
+
+hist(Expense_Budget_20240918$`Current Modified Budget Amount`,ylab= "Observations",xlab = "Dollars", main = ("Current Modified Budget Amount"))
+hist(Expense_Budget_20240918$`Financial Plan Amount`,ylab= "Observations",xlab = "Dollars",main = ("Financial Plan Amount"))
+hist(Expense_Budget_20240918$`Adopted Budget Amount`,ylab= "Observations",xlab = "Dollars",main = ("Adopted Budget Amount"))
+hist(Expense_Budget_20240918$`Financial Plan - Number of Contracts`,ylab= "Observations",xlab = "Number of Contracts",main = ("Financial Plan Number of Contracts"))
+
+CMB_median<- median(Expense_Budget_20240918$`Current Modified Budget Amount`)
+FP_median<-median(Expense_Budget_20240918$`Financial Plan Amount`)
+AB_median<-median(Expense_Budget_20240918$`Adopted Budget Amount`)
+FPC_median<-median(Expense_Budget_20240918$`Financial Plan - Number of Contracts`)
+
+CMB_median
+FP_median
+AB_median
+FPC_median
+
+Expense_Budget_20240918$`Current Modified Budget Amount`[Expense_Budget_20240918$`Current Modified Budget Amount`>100000000]<-CMB_median
+Expense_Budget_20240918$`Financial Plan Amount`[Expense_Budget_20240918$`Financial Plan Amount`>100000000]<-FP_median
+Expense_Budget_20240918$`Adopted Budget Amount`[Expense_Budget_20240918$`Adopted Budget Amount`>100000000]<-AB_median
+Expense_Budget_20240918$`Financial Plan - Number of Contracts`[Expense_Budget_20240918$`Financial Plan - Number of Contracts`>30]<-FPC_median
+
+Expense_Budget_20240918$`Current Modified Budget Amount`[Expense_Budget_20240918$`Current Modified Budget Amount`<1000]<-CMB_median
+Expense_Budget_20240918$`Financial Plan Amount`[Expense_Budget_20240918$`Financial Plan Amount`<100]<-FP_median
+Expense_Budget_20240918$`Adopted Budget Amount`[Expense_Budget_20240918$`Adopted Budget Amount`<100]<-AB_median
+
+
+hist(Expense_Budget_20240918$`Current Modified Budget Amount`,ylab= "Observations",xlab = "Dollars", main = ("Current Modified Budget Amount"))
+hist(Expense_Budget_20240918$`Financial Plan Amount`,ylab= "Observations",xlab = "Dollars",main = ("Financial Plan Amount"))
+hist(Expense_Budget_20240918$`Adopted Budget Amount`,ylab= "Observations",xlab = "Dollars",main = ("Adopted Budget Amount"))
+hist(Expense_Budget_20240918$`Financial Plan - Number of Contracts`,ylab= "Observations",xlab = "Number of Contracts",main = ("Financial Plan Number of Contracts"))
+
